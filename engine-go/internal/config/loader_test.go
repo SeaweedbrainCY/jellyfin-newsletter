d7a392e306046aea96df3dab7f056a88178fb67d
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-01-16 00:02:46 -0500

    fix: Fix computeNewYamlAfterPartRemoval logic after refactor

--- engine-go/internal/config/loader_test.go
+++ engine-go/internal/config/loader_test.go
@@ -121,23 +121,24 @@ func computeNewYamlAfterPartRemoval(
 	// We are in an interesting identation level
 
 	if strings.HasPrefix(lineTrimmed, (*fieldPath)[currentIdentNumber]+":") {
+		if currentIdentNumber == len(*fieldPath)-1 {
+			// We found the field or section.
+			// Not adding this line to final yaml and setting is deleting this level of identation to true
+			return computeNewYamlAfterPartRemoval(
+				newYaml,
+				baseYamlLines,
+				linePositionToParse+1,
+				fieldPath,
+				currentIdentNumber+1,
+				0,
+			)
+		}
 		// We are in a section not interesting
 		// If we are at root, search will continue
 		*newYaml = *newYaml + "\n" + line
 		return computeNewYamlAfterPartRemoval(newYaml, baseYamlLines, linePositionToParse+1, fieldPath, 0, 0)
 	}
-	if currentIdentNumber == len(*fieldPath)-1 {
-		// We found the field or section.
-		// Not adding this line to final yaml and setting is deleting this level of identation to true
-		return computeNewYamlAfterPartRemoval(
-			newYaml,
-			baseYamlLines,
-			linePositionToParse+1,
-			fieldPath,
-			currentIdentNumber+1,
-			0,
-		)
-	}
+
 	// We are moving up in the fieldPath but not there yet
 	*newYaml = *newYaml + "\n" + line
 	return computeNewYamlAfterPartRemoval(
