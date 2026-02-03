85a6499486c9637a2217391f731ba325e6e429f9
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-02-02 19:25:54 -0500

    feat: rename items.go in jellyfinItemsApi.go and define method of the new client interface

---
+++ engine-go/internal/jellyfin/jellyfinItemsApi.go
@@ -0,0 +1,97 @@
+package jellyfin
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/SeaweedbrainCY/jellyfin-newsletter/internal/app"
+	jellyfinAPI "github.com/sj14/jellyfin-go/api"
+	"go.uber.org/zap"
+)
+
+type JellyfinItemsAPI struct {
+	jellyfinAPI.ItemsAPI
+}
+
+type Nullable[T any] interface {
+	IsSet() bool
+	Get() *T
+}
+
+func (itemsAPI JellyfinItemsAPI) GetMoviesItemsByFolderID(
+	folderID string,
+	recursive bool,
+	app *app.ApplicationContext,
+) (*[]jellyfinAPI.BaseItemDto, error) {
+	movies, getMoviesHTTPResponse, err := itemsAPI.GetItems(context.Background()).
+		Recursive(recursive).
+		ParentId(folderID).
+		LocationTypes([]jellyfinAPI.LocationType{jellyfinAPI.LOCATIONTYPE_FILE_SYSTEM}).
+		IsMovie(true).
+		Fields([]jellyfinAPI.ItemFields{"DateCreated", "ProviderIds", "Id", "Name", "ProductionYear"}).
+		Execute()
+
+	if err != nil {
+		logHTTPResponseError(getMoviesHTTPResponse, err, app)
+		return nil, err
+	}
+	defer getMoviesHTTPResponse.Body.Close()
+	return &movies.Items, nil
+}
+
+func (itemsAPI JellyfinItemsAPI) GetAllItemsByFolderID(
+	folderID string,
+	app *app.ApplicationContext,
+) (*[]jellyfinAPI.BaseItemDto, error) {
+	items, httpResponse, err := itemsAPI.GetItems(context.Background()).
+		Recursive(true).
+		ParentId(folderID).
+		Fields([]jellyfinAPI.ItemFields{"DateCreated", "ProviderIds", "Id", "Name", "ProductionYear", "IndexNumber", "SeriesId", "Type", "SeasonId"}).
+		Execute()
+	if err != nil {
+		logHTTPResponseError(httpResponse, err, app)
+		return nil, err
+	}
+	return &items.Items, nil
+}
+
+func (itemsAPI JellyfinItemsAPI) GetRootFolderIDByName(folderName string, app *app.ApplicationContext) (string, error) {
+	foldersItems, httpResponse, err := itemsAPI.GetItems(context.Background()).
+		Recursive(false).
+		LocationTypes([]jellyfinAPI.LocationType{jellyfinAPI.LOCATIONTYPE_FILE_SYSTEM}).
+		Execute()
+	if err != nil {
+		logHTTPResponseError(httpResponse, err, app)
+		return "", err
+	}
+	defer httpResponse.Body.Close()
+	if !foldersItems.HasItems() {
+		app.Logger.Warn(
+			"No folders found. This could happen if Jellyfin has no collection or folder at all. Media should belong in folders but none are found.",
+		)
+		return "", ErrItemsNotFound
+	}
+	for _, item := range foldersItems.GetItems() {
+		if *item.Name.Get() == folderName {
+			return *item.Id, nil
+		}
+	}
+	app.Logger.Warn("Folder not found. Will ignore it.", zap.String("folderName", folderName))
+	return "", ErrItemsNotFound
+}
+
+func OrDefault[T any](n Nullable[T], def T) T {
+	if n.IsSet() && n.Get() != nil {
+		return *n.Get()
+	}
+	return def
+}
+
+func getTMDBIDIfExist(item *jellyfinAPI.BaseItemDto) int {
+	if value, ok := item.ProviderIds["Tmdb"]; ok {
+		if id, atoiErr := strconv.Atoi(value); atoiErr == nil {
+			return id
+		}
+	}
+	return 0
+}
