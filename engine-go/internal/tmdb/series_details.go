83bd2b5dbb33c9c4cb850acba68a5eea41239728
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-02-17 22:30:23 -0500

    feat: Add support for GetSeriesDetails of media without TMDB id

--- engine-go/internal/tmdb/series_details.go
+++ engine-go/internal/tmdb/series_details.go
@@ -1,18 +1,44 @@
 package tmdb
 
-import "github.com/SeaweedbrainCY/jellyfin-newsletter/internal/jellyfin"
+import (
+	"github.com/SeaweedbrainCY/jellyfin-newsletter/internal/app"
+	"github.com/SeaweedbrainCY/jellyfin-newsletter/internal/jellyfin"
+	"go.uber.org/zap"
+)
 
 func GetSeriesDetails(
 	tmdbAPIClient TMDBAPIClient,
 	jellyfinSeriesItem jellyfin.NewlyAddedSeriesItem,
-) (*ItemDetails, error) {
+	app app.ApplicationContext,
+) *ItemDetails {
 	if jellyfinSeriesItem.TMDBId != "" {
 		parsedHttpResponse, err := tmdbAPIClient.GetMediaByID(jellyfinSeriesItem.TMDBId, MediaTypeSeries)
 
 		if err != nil {
 			// Error is already logged by GetMediaByID
-			return nil, err
+			return getDefaultItemDetails()
 		}
-		return getItemDetailsFromHTTPResponse(parsedHttpResponse), nil
+
+		return getItemDetailsFromHTTPResponse(parsedHttpResponse)
+	} else {
+		// No TMDB id, we perform a search by name and select the item with the highest popularity
+		app.Logger.Debug(
+			"Series has no TMDB id. TMDB information will be retrieved by searching with Series's name. If several media match, the choice will be based on the highest popularity.",
+			zap.String("Series Name", jellyfinSeriesItem.SeriesName),
+			zap.String("Series ID", jellyfinSeriesItem.SeriesID),
+		)
+
+		searchResult, err := tmdbAPIClient.SearchMediaByName(
+			jellyfinSeriesItem.SeriesName,
+			jellyfinSeriesItem.ProductionYear,
+			MediaTypeSeries,
+		)
+
+		if err != nil {
+			// Error is already logged by SearchMediaByName
+			return getDefaultItemDetails()
+		}
+
+		return getItemDetailsFromSearchResult(searchResult)
 	}
 }
