2c753e576b585fd47889e60694754767e0384a3c
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-02-17 22:29:40 -0500

    feat: Add getItemDetailsFromSearchResult method to retrieve item from seqrch result based on popularity

--- engine-go/internal/tmdb/item_details.go
+++ engine-go/internal/tmdb/item_details.go
@@ -5,16 +5,39 @@ type ItemDetails struct {
 	PosterURL string
 }
 
-func getItemDetailsFromHTTPResponse(parsedHttpResponse *GetMediaHTTPResponse) *ItemDetails {
-	itemDetails := ItemDetails{
+func getDefaultItemDetails() *ItemDetails {
+	return &ItemDetails{
 		Overview:  "No description available.",
 		PosterURL: "https://placehold.co/200",
 	}
+}
+
+func getItemDetailsFromHTTPResponse(parsedHttpResponse *GetMediaHTTPResponse) *ItemDetails {
+	itemDetails := getDefaultItemDetails()
 	if parsedHttpResponse.Overview != "" {
 		itemDetails.Overview = parsedHttpResponse.Overview
 	}
 	if parsedHttpResponse.PosterPath != "" {
 		itemDetails.PosterURL = "https://image.tmdb.org/t/p/w500" + parsedHttpResponse.PosterPath
 	}
-	return &itemDetails
+	return itemDetails
+}
+
+// Parse result from the TMDB search based on name
+// The final item will be selected based on popularity.
+func getItemDetailsFromSearchResult(result *SearchMediaHTTPResponse) *ItemDetails {
+	itemDetails := getDefaultItemDetails()
+	popularity := 0.0
+	for _, item := range result.Results {
+		if item.Popularity > popularity {
+			if item.Overview != "" {
+				itemDetails.Overview = item.Overview
+			}
+			if item.PosterPath != "" {
+				itemDetails.PosterURL = "https://image.tmdb.org/t/p/w500" + item.PosterPath
+			}
+			popularity = item.Popularity
+		}
+	}
+	return itemDetails
 }
