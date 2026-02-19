d076c76bfb87ce9e07b63afdd4d66d83ddf4038a
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-02-18 20:05:09 -0500

    refactor: Merge api.go with api_client.go

--- engine-go/internal/tmdb/api.go
+++ engine-go/internal/tmdb/api.go
@@ -8,8 +8,28 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/SeaweedbrainCY/jellyfin-newsletter/internal/app"
 	"go.uber.org/zap"
 )
+
+type APIInterface interface {
+	GetMediaByID(id string, mediaType MediaType) (*GetMediaHTTPResponse, error)
+	GetMediaByTitle(mediaName string, mediaType MediaType)
+}
+
+type APIClient struct {
+	APIKey string
+	Lang   string
+	Logger *zap.Logger
+}
+
+func InitTMDBApiClient(app *app.ApplicationContext) APIClient {
+	return APIClient{
+		APIKey: app.Config.TMDB.APIKey,
+		Lang:   app.Config.EmailTemplate.Language,
+		Logger: app.Logger,
+	}
+}
 
 type GetMediaHTTPResponse struct {
 	Overview   string `json:"overview"`
