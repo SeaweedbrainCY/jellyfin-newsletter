6ea183ecef2d3b42f6e2db19f0c09d5e07a74480
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-03-09 00:31:24 -0400

    tests: fix items sorting and randomize baseItem order

--- engine-go/internal/template/builder_test.go
+++ engine-go/internal/template/builder_test.go
@@ -22,15 +22,6 @@ func timePtr(t time.Time) *time.Time {
 func getJellyfinNewMovies() []jellyfin.MovieItem {
 	return []jellyfin.MovieItem{
 		{
-			ID:             "7dcf7149-f710-46d5-a50c-626e3486259b",
-			Name:           "Star Wars: Episode II - Attack of the Clones",
-			AdditionDate:   timePtr(time.Date(2026, 01, 01, 01, 01, 0, 0, time.UTC)),
-			TMDBId:         "1274",
-			ProductionYear: int32(2025),
-			Overview:       "Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi investigate a mysterious plot into the heart of the Separatist movement and the beginning of the Clone Wars.",
-			PosterURL:      "https://image.tmdb.org/t/p/w500/oZNPzxqM2s5DyVWab09NTQScDQt.jpg",
-		},
-		{
 			ID:             "fd9416da-9026-4219-95b4-0dae418d2b5d",
 			Name:           "Oppenheimer",
 			AdditionDate:   timePtr(time.Date(2026, 01, 02, 01, 01, 0, 0, time.UTC)),
@@ -39,22 +30,20 @@ func getJellyfinNewMovies() []jellyfin.MovieItem {
 			Overview:       "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
 			PosterURL:      "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
 		},
+		{
+			ID:             "7dcf7149-f710-46d5-a50c-626e3486259b",
+			Name:           "Star Wars: Episode II - Attack of the Clones",
+			AdditionDate:   timePtr(time.Date(2026, 01, 01, 01, 01, 0, 0, time.UTC)),
+			TMDBId:         "1274",
+			ProductionYear: int32(2025),
+			Overview:       "Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi investigate a mysterious plot into the heart of the Separatist movement and the beginning of the Clone Wars.",
+			PosterURL:      "https://image.tmdb.org/t/p/w500/oZNPzxqM2s5DyVWab09NTQScDQt.jpg",
+		},
 	}
 }
 
 func getJellyfinNewSeriesItems() []jellyfin.NewlyAddedSeriesItem {
 	return []jellyfin.NewlyAddedSeriesItem{
-		{
-			SeriesName:     "Game of thrones",
-			SeriesID:       "3d7b0576-370c-48d3-b7c3-7c49f612afc9",
-			IsSeriesNew:    true,
-			NewSeasons:     nil,
-			TMDBId:         "1735",
-			ProductionYear: 2021,
-			AdditionDate:   time.Date(2026, 01, 03, 01, 01, 0, 0, time.UTC),
-			Overview:       "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
-			PosterURL:      "https://image.tmdb.org/t/p/w500/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
-		},
 		{
 			SeriesName:  "How I Met Your Mother",
 			SeriesID:    "c828b892-64f8-4def-88b7-dc3d9072a147",
@@ -152,6 +141,17 @@ func getJellyfinNewSeriesItems() []jellyfin.NewlyAddedSeriesItem {
 			Overview:       "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
 			PosterURL:      "https://image.tmdb.org/t/p/w500/3PFsEuAiyLkWsP4GG6dIV37Q6gu.jpg",
 		},
+		{
+			SeriesName:     "Game of thrones",
+			SeriesID:       "3d7b0576-370c-48d3-b7c3-7c49f612afc9",
+			IsSeriesNew:    true,
+			NewSeasons:     nil,
+			TMDBId:         "1735",
+			ProductionYear: 2021,
+			AdditionDate:   time.Date(2026, 01, 03, 01, 01, 0, 0, time.UTC),
+			Overview:       "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+			PosterURL:      "https://image.tmdb.org/t/p/w500/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
+		},
 	}
 }
 
@@ -165,57 +165,63 @@ func getExpectedNewMediaTemplateData() newMediaTemplateData {
 
 	newMovies := []newMovieItemTemplateData{
 		{
-			PosterURL:    "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-			Name:         "Oppenheimer",
-			AdditionDate: "2026-01-02",
-			Overview:     "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
-			AddedOnLabel: "Added on",
+			PosterURL:            "https://image.tmdb.org/t/p/w500/oZNPzxqM2s5DyVWab09NTQScDQt.jpg",
+			Name:                 "Star Wars: Episode II - Attack of the Clones",
+			AdditionDate:         "2026-01-01",
+			Overview:             "Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi investigate a mysterious plot into the heart of the Separatist movement and the beginning of the Clone Wars.",
+			AddedOnLabel:         "Added on",
+			IncludeItemOverviews: true,
 		},
 		{
-			PosterURL:    "https://image.tmdb.org/t/p/w500/oZNPzxqM2s5DyVWab09NTQScDQt.jpg",
-			Name:         "Star Wars: Episode II - Attack of the Clones",
-			AdditionDate: "2026-01-01",
-			Overview:     "Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi investigate a mysterious plot into the heart of the Separatist movement and the beginning of the Clone Wars.",
-			AddedOnLabel: "Added on",
+			PosterURL:            "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+			Name:                 "Oppenheimer",
+			AdditionDate:         "2026-01-02",
+			Overview:             "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+			AddedOnLabel:         "Added on",
+			IncludeItemOverviews: true,
 		},
 	}
 
 	newSeries := []newSeriesItemTemplateData{
 		{
-			// Old series, new episodes in 1 season
-			PosterURL:      "https://image.tmdb.org/t/p/w500/3PFsEuAiyLkWsP4GG6dIV37Q6gu.jpg",
-			SeriesName:     "Family Guy",
-			AdditionDate:   "2026-01-05",
-			Overview:       "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
-			AddedOnLabel:   "Added on",
-			NewSeriesTitle: "Family Guy: Season 24, Episodes 1-3 & 7",
-		},
-		{
 			// Whole new series
-			PosterURL:      "https://image.tmdb.org/t/p/w500/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
-			SeriesName:     "Game of thrones",
-			AdditionDate:   "2026-01-03",
-			Overview:       "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
-			AddedOnLabel:   "Added on",
-			NewSeriesTitle: "Game of thrones: Season 1-8",
-		},
-		{
-			// Old series, new season
-			PosterURL:      "https://image.tmdb.org/t/p/w500/b34jPzmB0wZy7EjUZoleXOl2RRI.jpg",
-			SeriesName:     "How I Met Your Mother",
-			AdditionDate:   "2026-01-06",
-			Overview:       "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother.",
-			AddedOnLabel:   "Added on",
-			NewSeriesTitle: "How I Met Your Mother: Season 9",
+			PosterURL:            "https://image.tmdb.org/t/p/w500/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
+			SeriesName:           "Game of thrones",
+			AdditionDate:         "2026-01-03",
+			Overview:             "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+			AddedOnLabel:         "Added on",
+			NewSeriesTitle:       "Game of thrones: Seasons 1-8",
+			IncludeItemOverviews: true,
 		},
 		{
 			// Old series, new episodes in 2 seasons
-			PosterURL:      "https://image.tmdb.org/t/p/w500/uOOtwVbSr4QDjAGIifLDwpb2Pdl.jpg",
-			SeriesName:     "Stranger Things",
-			AdditionDate:   "2026-01-04",
-			Overview:       "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
-			AddedOnLabel:   "Added on",
-			NewSeriesTitle: "Stranger Things: Season 1-2",
+			PosterURL:            "https://image.tmdb.org/t/p/w500/uOOtwVbSr4QDjAGIifLDwpb2Pdl.jpg",
+			SeriesName:           "Stranger Things",
+			AdditionDate:         "2026-01-04",
+			Overview:             "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
+			AddedOnLabel:         "Added on",
+			NewSeriesTitle:       "Stranger Things: Seasons 1-2",
+			IncludeItemOverviews: true,
+		},
+		{
+			// Old series, new episodes in 1 season
+			PosterURL:            "https://image.tmdb.org/t/p/w500/3PFsEuAiyLkWsP4GG6dIV37Q6gu.jpg",
+			SeriesName:           "Family Guy",
+			AdditionDate:         "2026-01-05",
+			Overview:             "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+			AddedOnLabel:         "Added on",
+			NewSeriesTitle:       "Family Guy: Season 24, Episodes 1-3 & 7",
+			IncludeItemOverviews: true,
+		},
+		{
+			// Old series, new season
+			PosterURL:            "https://image.tmdb.org/t/p/w500/b34jPzmB0wZy7EjUZoleXOl2RRI.jpg",
+			SeriesName:           "How I Met Your Mother",
+			AdditionDate:         "2026-01-06",
+			Overview:             "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother.",
+			AddedOnLabel:         "Added on",
+			NewSeriesTitle:       "How I Met Your Mother: Season 9",
+			IncludeItemOverviews: true,
 		},
 	}
 
@@ -308,7 +314,7 @@ func TestBuildNewMediaTemplateData(t *testing.T) {
 			require.NoError(t,
 				err)
 			assert.Empty(t, recordedLogs)
-			assert.Equal(t, expectedTemplateData, templateData)
+			assert.Equal(t, expectedTemplateData, *templateData)
 		})
 	}
 }
