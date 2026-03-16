9d145096c76ab71b2f3d929db5d7b658cc800cfc
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-03-15 22:06:06 -0400

    tests: Add name sorting coverage (desc and asc)

--- engine-go/internal/template/builder_test.go
+++ engine-go/internal/template/builder_test.go
@@ -409,6 +409,164 @@ func TestBuildNewMediaTemplateData(t *testing.T) {
 			movieCount:                54,
 			episodeCount:              1253,
 		},
+		{
+			name: "Sorting, name asc",
+			getAppContextFunc: func() (*app.ApplicationContext, *observer.ObservedLogs) {
+				app, obs := getAppContext()
+				app.Config.EmailTemplate.SortMode = "name_asc"
+				return app, obs
+			},
+			getExpectedNewMediaTemplateDataFunc: func() newMediaTemplateData {
+				expected := getExpectedNewMediaTemplateData()
+				newMovies := []newMovieItemTemplateData{
+					{
+						PosterURL:            "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+						Name:                 "Oppenheimer",
+						AdditionDate:         "2026-01-02",
+						Overview:             "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+						AddedOnLabel:         "Added on",
+						IncludeItemOverviews: true,
+					},
+					{
+						PosterURL:            "https://image.tmdb.org/t/p/w500/oZNPzxqM2s5DyVWab09NTQScDQt.jpg",
+						Name:                 "Star Wars: Episode II - Attack of the Clones",
+						AdditionDate:         "2026-01-01",
+						Overview:             "Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi investigate a mysterious plot into the heart of the Separatist movement and the beginning of the Clone Wars.",
+						AddedOnLabel:         "Added on",
+						IncludeItemOverviews: true,
+					},
+				}
+
+				newSeries := []newSeriesItemTemplateData{
+					{
+						// Old series, new episodes in 1 season
+						PosterURL:            "https://image.tmdb.org/t/p/w500/3PFsEuAiyLkWsP4GG6dIV37Q6gu.jpg",
+						SeriesName:           "Family Guy",
+						AdditionDate:         "2026-01-05",
+						Overview:             "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "Family Guy: Season 24, Episodes 1-3 & 7",
+						IncludeItemOverviews: true,
+					},
+					{
+						// Whole new series
+						PosterURL:            "https://image.tmdb.org/t/p/w500/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
+						SeriesName:           "Game of thrones",
+						AdditionDate:         "2026-01-03",
+						Overview:             "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "Game of thrones",
+						IncludeItemOverviews: true,
+					},
+					{
+						// Old series, new season
+						PosterURL:            "https://image.tmdb.org/t/p/w500/b34jPzmB0wZy7EjUZoleXOl2RRI.jpg",
+						SeriesName:           "How I Met Your Mother",
+						AdditionDate:         "2026-01-06",
+						Overview:             "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "How I Met Your Mother: Season 9",
+						IncludeItemOverviews: true,
+					},
+					{
+						// Old series, new episodes in 2 seasons
+						PosterURL:            "https://image.tmdb.org/t/p/w500/uOOtwVbSr4QDjAGIifLDwpb2Pdl.jpg",
+						SeriesName:           "Stranger Things",
+						AdditionDate:         "2026-01-04",
+						Overview:             "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "Stranger Things: Seasons 1-2",
+						IncludeItemOverviews: true,
+					},
+				}
+				expected.NewMovies = newMovies
+				expected.NewSeries = newSeries
+				return expected
+			},
+			getJellyfinNewMovies:      getJellyfinNewMovies,
+			getJellyfinNewSeriesItems: getJellyfinNewSeriesItems,
+			movieCount:                54,
+			episodeCount:              1253,
+		},
+		{
+			name: "Sorting, name asc",
+			getAppContextFunc: func() (*app.ApplicationContext, *observer.ObservedLogs) {
+				app, obs := getAppContext()
+				app.Config.EmailTemplate.SortMode = "name_desc"
+				return app, obs
+			},
+			getExpectedNewMediaTemplateDataFunc: func() newMediaTemplateData {
+				expected := getExpectedNewMediaTemplateData()
+				newMovies := []newMovieItemTemplateData{
+					{
+						PosterURL:            "https://image.tmdb.org/t/p/w500/oZNPzxqM2s5DyVWab09NTQScDQt.jpg",
+						Name:                 "Star Wars: Episode II - Attack of the Clones",
+						AdditionDate:         "2026-01-01",
+						Overview:             "Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi investigate a mysterious plot into the heart of the Separatist movement and the beginning of the Clone Wars.",
+						AddedOnLabel:         "Added on",
+						IncludeItemOverviews: true,
+					},
+					{
+						PosterURL:            "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+						Name:                 "Oppenheimer",
+						AdditionDate:         "2026-01-02",
+						Overview:             "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+						AddedOnLabel:         "Added on",
+						IncludeItemOverviews: true,
+					},
+				}
+
+				newSeries := []newSeriesItemTemplateData{
+					{
+						// Old series, new episodes in 2 seasons
+						PosterURL:            "https://image.tmdb.org/t/p/w500/uOOtwVbSr4QDjAGIifLDwpb2Pdl.jpg",
+						SeriesName:           "Stranger Things",
+						AdditionDate:         "2026-01-04",
+						Overview:             "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "Stranger Things: Seasons 1-2",
+						IncludeItemOverviews: true,
+					},
+					{
+						// Old series, new season
+						PosterURL:            "https://image.tmdb.org/t/p/w500/b34jPzmB0wZy7EjUZoleXOl2RRI.jpg",
+						SeriesName:           "How I Met Your Mother",
+						AdditionDate:         "2026-01-06",
+						Overview:             "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "How I Met Your Mother: Season 9",
+						IncludeItemOverviews: true,
+					},
+					{
+						// Whole new series
+						PosterURL:            "https://image.tmdb.org/t/p/w500/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
+						SeriesName:           "Game of thrones",
+						AdditionDate:         "2026-01-03",
+						Overview:             "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "Game of thrones",
+						IncludeItemOverviews: true,
+					},
+					{
+						// Old series, new episodes in 1 season
+						PosterURL:            "https://image.tmdb.org/t/p/w500/3PFsEuAiyLkWsP4GG6dIV37Q6gu.jpg",
+						SeriesName:           "Family Guy",
+						AdditionDate:         "2026-01-05",
+						Overview:             "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+						AddedOnLabel:         "Added on",
+						NewSeriesTitle:       "Family Guy: Season 24, Episodes 1-3 & 7",
+						IncludeItemOverviews: true,
+					},
+				}
+				expected.NewMovies = newMovies
+				expected.NewSeries = newSeries
+				return expected
+			},
+			getJellyfinNewMovies:      getJellyfinNewMovies,
+			getJellyfinNewSeriesItems: getJellyfinNewSeriesItems,
+			movieCount:                54,
+			episodeCount:              1253,
+		},
 	}
 
 	for _, test := range tests {
