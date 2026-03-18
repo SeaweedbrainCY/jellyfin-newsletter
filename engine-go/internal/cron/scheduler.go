a50909090469deb1d3273377828f0470c6e9c4e0
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-03-18 16:39:39 -0400

    feat: Add scheduler and cron job creation

---
+++ engine-go/internal/cron/scheduler.go
@@ -0,0 +1,30 @@
+package cron
+
+import (
+	"github.com/SeaweedbrainCY/jellyfin-newsletter/internal/app"
+	"github.com/SeaweedbrainCY/jellyfin-newsletter/internal/newsletter"
+	"github.com/go-co-op/gocron/v2"
+	"go.uber.org/zap"
+)
+
+func CreateNewsletterScheduler(app *app.ApplicationContext) (*gocron.Scheduler, error) {
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		return nil, err
+	}
+
+	job, err := scheduler.NewJob(gocron.CronJob(app.Config.Scheduler.CronExpr, false), gocron.NewTask(newsletter.TriggerNewsletterWorkflow, app))
+	if err != nil {
+		return nil, err
+	}
+
+	jobNextRunStr := "Unknown"
+	nextRunDatetime, nextRunErr := job.NextRun()
+	if nextRunErr == nil {
+		jobNextRunStr = nextRunDatetime.Format("2006-01-02T15:04:05Z07:00")
+	}
+
+	app.Logger.Info("Scheduler created.", zap.String("job_id", job.ID().String()), zap.String("Cron expression", app.Config.Scheduler.CronExpr), zap.String("Next run", jobNextRunStr))
+
+	return &scheduler, nil
+}
