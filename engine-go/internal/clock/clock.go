ed9c7e1232890436bdd48f4ecda5966c993505e7
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-04-01 16:07:11 +0200

    feature: Create a Clock interface to inject clock/time depedency

---
+++ engine-go/internal/clock/clock.go
@@ -0,0 +1,16 @@
+package clock
+
+import "time"
+
+// clock package is used to inject time dependency accross the app.
+// It is mainly use to mock time.Now()
+
+type ClockInterface interface {
+	Now() time.Time
+}
+
+type RealClock struct{}
+
+func (_ RealClock) Now() time.Time {
+	return time.Now()
+}
