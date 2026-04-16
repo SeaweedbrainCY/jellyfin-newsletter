b2eda49437233f8042ec5ecffead1e67a7fb8353
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2026-04-16 12:26:47 +0200

    feat: Run newsletter method now require an option (nil) gocron job arg
    This gocron job arg describe the job that is potentially currently runnning the workflow. If there is no job, the value should be nil.
    It is mainly used for logging purposes

--- engine-go/internal/newsletter/newsletter.go
+++ engine-go/internal/newsletter/newsletter.go
@@ -17,7 +17,8 @@ type Workflow struct {
 }
 
 // Run connects to Jellyfin to retrieve the latest items and send the newsletter to the configured recipients.
-func (workflow Workflow) Run(app *app.ApplicationContext) {
+// cronjob is optional and should be nil if the workflow is not called by a scheduled job. It is mainly used for logging purposes
+func (workflow Workflow) Run(app *app.ApplicationContext, cronjob *gocron.Job) {
 	app.Logger.Info("Gathering new items and sending the newsletter ...")
 	err := workflow.JellyfinClient.TestConnection(app)
 	if err != nil {
